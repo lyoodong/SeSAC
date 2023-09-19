dbcfeede828205c37d13f446a6a18f004e5bb22c
lyoodong <dwryu3079@naver.com>
2023-09-19 22:42:19 +0900

    1. 메서드 생성

---
+++ 17. BearAPIAbstraction/BearAPIAbstraction/APIManager.swift
@@ -0,0 +1,27 @@
+//
+//  APIManager.swift
+//  BearAPIAbstraction
+//
+//  Created by Dongwan Ryoo on 2023/09/19.
+//
+
+import Foundation
+
+class APIManager {
+    
+    static let shared = APIManager()
+    private init() { }
+    
+    func getBeers() {
+        
+    }
+    
+    func getSingleBeers() {
+        
+    }
+    
+    func getRandomBeers() {
+        
+    }
+    
+}
---
+++ 17. BearAPIAbstraction/BearAPIAbstraction/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BearAPIAbstraction
+//
+//  Created by Dongwan Ryoo on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
